d81102eb3d07962502653fb399f0e26364a21bb7
kaneshak79 <kaneshak79@gmail.com>
2026-02-25 09:49:24 +0530

    first commit

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/auth"
+  baseURL: "https://password-backend-final.onrender.com/api/auth"
+    
 });
 
 export default api;
